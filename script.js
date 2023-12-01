24c248979b7f9a34408d5bbe94008ad2baa934fc
abhishekjadhav3470 <abhishekjadhav3470@gmail.com>
2023-12-02 00:38:53 +0530

    script

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-console.log("script running")
-document.querySelector('')
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("script running");
+
+    document.querySelector(".cross").style.display = "none";
+
+    document.querySelector(".hamburger").addEventListener("click", () => {
+        document.querySelector(".sidebar").classList.toggle("sidebarGo");
+    });
+});
