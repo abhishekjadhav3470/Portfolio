f457d4c4a82446bb1c4ffbbca1841a811b16c4a6
abhishekjadhav3470 <abhishekjadhav3470@gmail.com>
2023-12-02 01:21:37 +0530

    conditions

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("script running");
+console.log("script running");
 
-    document.querySelector(".cross").style.display = "none";
+document.querySelector(".cross").style.display = "none";
 
-    document.querySelector(".hamburger").addEventListener("click", () => {
-        document.querySelector(".sidebar").classList.toggle("sidebarGo");
-    });
+document.querySelector(".hamburger").addEventListener("click", () => {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('sidebarGo');
+
+    if (sidebar.classList.contains('sidebarGo')) {
+        document.querySelector('.ham').style.display = "inline";
+        document.querySelector('.cross').style.display = "none";
+    } else {
+        document.querySelector('.ham').style.display = "none";
+        setTimeout(() => {
+            document.querySelector('.cross').style.display = "inline";
+
+        }, 320);
+        
+        // If the sidebar doesn't have the 'sidebarGo' class, you can add logic here if needed
+    }
 });
